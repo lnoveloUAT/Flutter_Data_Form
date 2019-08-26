9fcf7e8ba3889ac47c5dfabaa9c03de535ea0a99
lnovelo <lnovelo@uat.edu>
2019-08-26 13:57:04 -0700

    Format changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_firestore/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_firestore/main.dart';
+// import 'package:flutter_firestore/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
